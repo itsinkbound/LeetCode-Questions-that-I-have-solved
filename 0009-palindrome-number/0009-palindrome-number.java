37cb09395c50ac53dd2dd80b18e5b6121d55a03a
Krishna Priyadarshan Behara <kpbehara10@gmail.com>
2026-08-01 12:40:18 +0530

    Time: 5 ms (80.94%), Space: 46.1 MB (34.04%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int n = x;
+        int rev =0;
+        if(x<0){
+            return false;
+        }
+        while(n!=0){
+            int digit = n%10;
+            rev = rev*10 + digit;
+            n/=10;
+        }
+        if(rev==x){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+}
