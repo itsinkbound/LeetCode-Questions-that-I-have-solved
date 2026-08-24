b450596b3bd88e769ce28daacf999e054569a485
Krishna Priyadarshan Behara <kpbehara10@gmail.com>
2026-08-24 10:38:26 +0530

    Time: 1 ms (97.4%), Space: 47.7 MB (43.08%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,24 +1,34 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> iset = new HashSet<>();
-        Set<Integer> jset = new HashSet<>();
         int m= matrix.length;
         int n=matrix[0].length;
+        int space = 1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    iset.add(i);
-                    jset.add(j);
+                    matrix[0][j]=0;
+                    if(i==0){
+                        space=0;
+                    }else{
+                        matrix[i][0]=0;
+                    }
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(iset.contains(i) || jset.contains(j)){
+        // creates the in memory check sets 
+        // now to transform the matrix
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j] = 0;
-                    System.out.println("okay");
                 }
             }
+        }
+        if(matrix[0][0]==0){
+            for(int i=0;i<m;i++) matrix[i][0]=0;
+        }
+        if(space==0){
+            for(int i=0;i<n;i++) matrix[0][i]=0;
         }
     }
 }
