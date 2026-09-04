99f32c0164d53498db7a913f246938d7bb8adb19
Krishna Priyadarshan Behara <kpbehara10@gmail.com>
2026-09-04 22:15:47 +0530

    Time: 412 ms (99.85%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,9 @@
 # Write your MySQL query statement below
-select name from Customer
-where referee_id is null or referee_id != 2;
+-- select name from Customer
+-- where referee_id is null or referee_id != 2;
+
+#Coalesce is something that we can note
+#used to get non null values, what we can do is get the null values and change it to zeroes.
+
+select name from customer
+where coalesce(referee_id,0) != 2;
